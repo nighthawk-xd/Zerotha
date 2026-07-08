9b415e10fe1ae1d6282d77dfa851e4b7cba0f01a
NightHawk <vishnugupta87584@gmail.com>
2026-07-09 01:45:34 +0530

    Add basename to BrowserRouter for /dashboard subpath routing

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -27,7 +27,7 @@ if (!token) {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dashboard">
       <Routes>
         <Route path="/*" element={<Home />} />
       </Routes>
